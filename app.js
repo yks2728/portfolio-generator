526fe8a02b5753cb88eb2579ff0f63d9a7a75106
Zack Greenfield <zackg007@verizon.net>
2022-01-19 10:58:31 -0500

    added HTML and page template js file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-    // This...
-    for (let i = 0; i <profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = (profileDataArr) => {
+//     // This...
+//     for (let i = 0; i <profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('================');
+//     console.log('================');
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
